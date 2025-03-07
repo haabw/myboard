4e1bd25627a6570b0cc44a028a1996ac0eb27842
haabw <yhb349@naver.com>
2025-03-07 14:36:17 +0900

    Add files via upload

---
+++ httpServer.js
@@ -0,0 +1,15 @@
+const http = require('http') // Importing the http module
+
+const hostname = '127.0.0.1';
+const port = 3000;
+
+// Creating a server - createSever((요청변수, 응답변수)=>{}) method
+const server = http.createServer((req, res)=>{
+  res.statusCode = 200; // HTTP 상태 코드 - 200 : tjdrhd
+  res.setHeader('Content-Type', 'text/plain'); // 헤더 설정
+  res.end('Hello World\n'); // 응답 데이터
+}); // Creating a server
+
+server.listen(port, hostname, ()=>{
+  console.log(`Sever running at ${hostname}:${port}/`);
+}); // 서버 실행
